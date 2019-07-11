8b9f39fbbaa15d76ee8ef7a51d3edaee3e776a29
Ichicoro <me@marte.dev>
2019-07-11 03:53:25 +0200

    Split up the code into a couple of files, and update the build script as necessary

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
+riscv64-unknown-elf-as -g -o main.o main.S
 riscv64-unknown-elf-as -g -o quicksort.o quicksort.S
 riscv64-unknown-elf-as -g -o itoa.o itoa.S
-riscv64-unknown-elf-ld -o quicksort quicksort.o itoa.o
+riscv64-unknown-elf-ld -o quicksort main.o quicksort.o itoa.o
 rm -r *.o
